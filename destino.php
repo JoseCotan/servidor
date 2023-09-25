23fbae1873f665b4d52d3a396acb3ddf561d6ed7
José Luis Cotán <joseluis.cotan@iesdonana.org>
2023-09-25 19:14:32 +0200

    Añadido pequeño formulario de los datos recogidos por GET

--- destino.php
+++ destino.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Respuesta</title>
 </head>
+
 <body>
     <?php
-    echo $_GET['dni'];
+    if (
+        !empty($_GET['dni']) &&
+        !empty($_GET['nombre']) &&
+        !empty($_GET['apellidos']) &&
+        !empty($_GET['direccion']) &&
+        !empty($_GET['telefono'])
+    ) {
+        $dni = $_GET['dni'];
+        $nombre = $_GET['nombre'];
+        $apellidos = $_GET['apellidos'];
+        $direccion = $_GET['direccion'];
+        $telefono = $_GET['telefono'];
+        echo "<p>Dni: $dni</p>";
+        echo "<p>Nombre: $nombre</p>";
+        echo "<p>Apellidos: $apellidos</p>";
+        echo "<p>Dirección: $direccion</p>";
+        echo "<p>Teléfono: $telefono</p>";
+    } else {
+        echo "No se han proporcionado todos los datos.";
+    }
+
     ?>
 </body>
+
 </html>
